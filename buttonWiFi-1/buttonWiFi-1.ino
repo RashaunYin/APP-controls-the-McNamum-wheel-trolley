0813c9413b236b0a84c5cf3f493fb553d7ed0bd1
1522448928@qq.com <1522448928@qq.com>
2020-11-10 13:45:12 +0800

    first commit

---
+++ buttonWiFi-1/buttonWiFi-1.ino
@@ -0,0 +1,215 @@
+#define BLINKER_WIFI
+ 
+#include <Blinker.h>
+ 
+char auth[] = "f437335f2216";
+char ssid[] = "小米手机";
+char pswd[] = "159357258";
+
+#define BUTTON_1 "btn-fys"    //前进
+#define BUTTON_2 "btn-asg"    //后退
+#define BUTTON_3 "btn-lie"    //右转
+#define BUTTON_4 "btn-ls9"    //左转
+
+int motorL1 = 16; //D0左边轮子
+int motorL2 = 5;  //D1
+int motorR1 = 13; //D7右边轮子
+int motorR2 = 15; //D8
+
+BlinkerButton Button1(BUTTON_1);
+BlinkerButton Button2(BUTTON_2);
+BlinkerButton Button3(BUTTON_3);
+BlinkerButton Button4(BUTTON_4);
+
+void button1_callback(const String & state)
+{
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    BLINKER_LOG("get button state: ", state);
+ 
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Button tap!");
+ 
+        Button1.icon("icon_1");
+        Button1.color("#FFFFFF");
+        Button1.text("前进");
+        Button1.print();
+        forward();
+        delay(200);
+        lull();
+        
+    }
+    else if (state == BLINKER_CMD_BUTTON_PRESSED) {
+        BLINKER_LOG("Button pressed!");
+ 
+        Button1.icon("icon_1");
+        Button1.color("#FFFFFF");
+        Button1.text("连续前进");
+        Button1.print();
+        forward();
+    }
+    
+}
+ 
+void button2_callback(const String & state)
+{
+
+    BLINKER_LOG("get button state: ", state);
+ 
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Button tap!");
+     
+        Button2.icon("icon_1");
+        Button2.color("#FFFFFF");
+        Button2.text("后退");
+        Button2.print();
+        back();  
+        delay(200); 
+        lull();
+    }
+    else if (state == BLINKER_CMD_BUTTON_PRESSED) {
+        BLINKER_LOG("Button pressed!");
+ 
+        Button2.icon("icon_1");
+        Button2.color("#FFFFFF");
+        Button2.text("连续后退");
+        Button2.print();
+        back();  
+        
+    }
+}
+
+void button3_callback(const String & state)
+{
+
+    BLINKER_LOG("get button state: ", state);
+ 
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Button tap!");
+ 
+        Button3.icon("icon_1");
+        Button3.color("#FFFFFF");
+        Button3.text("右转");
+        Button3.print();
+        R();
+        delay(200); 
+        lull();
+    }
+    else if (state == BLINKER_CMD_BUTTON_PRESSED) {
+        BLINKER_LOG("Button pressed!");
+ 
+        Button3.icon("icon_1");
+        Button3.color("#FFFFFF");
+        Button3.text("连续右转");
+        Button3.print();
+        R();
+    }
+    
+}
+
+void button4_callback(const String & state)
+{
+ 
+    BLINKER_LOG("get button state: ", state);
+ 
+    if (state == BLINKER_CMD_BUTTON_TAP) {
+        BLINKER_LOG("Button tap!");
+ 
+        Button4.icon("icon_1");
+        Button4.color("#FFFFFF");
+        Button4.text("左转");
+        Button4.print();
+        L();
+        delay(200); 
+        lull();
+    }
+    else if (state == BLINKER_CMD_BUTTON_PRESSED) {
+        BLINKER_LOG("Button pressed!");
+ 
+        Button4.icon("icon_1");
+        Button4.color("#FFFFFF");
+        Button4.text("连续左转");
+        Button4.print();
+        L();
+    }
+  
+}
+
+void dataRead(const String & data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+ 
+    Blinker.vibrate();
+    
+    uint32_t BlinkerTime = millis();
+    Blinker.print(BlinkerTime);
+    Blinker.print("millis", BlinkerTime);
+}
+ 
+void setup()
+{
+    Serial.begin(115200);
+    BLINKER_DEBUG.stream(Serial);
+ 
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    pinMode(motorL1, OUTPUT);
+    pinMode(motorL2, OUTPUT);
+    pinMode(motorR1, OUTPUT);
+    pinMode(motorR2, OUTPUT);
+ 
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+    Button1.attach(button1_callback);
+    Button2.attach(button2_callback);
+    Button3.attach(button3_callback);
+    Button4.attach(button4_callback);
+    
+}
+ 
+void loop()
+{
+    Blinker.run();
+}
+
+void forward() {
+  analogWrite(motorL1, 700);
+  digitalWrite(motorL2, LOW);
+  digitalWrite(motorR1, LOW);
+  analogWrite(motorR2, 700);
+}
+ 
+void L() {
+  
+  digitalWrite(motorL1, LOW);
+  analogWrite(motorL2, 700);
+  digitalWrite(motorR1, LOW);
+  analogWrite(motorR2, 700);
+ 
+ 
+}
+void R() {
+   
+  analogWrite(motorL1, 700);
+  digitalWrite(motorL2, LOW);
+  analogWrite(motorR1, 700);
+  digitalWrite(motorR2, LOW);
+ 
+}
+ 
+ 
+void back() { //后退函数
+   
+  digitalWrite(motorL1, LOW);
+  analogWrite(motorL2, 700);
+  analogWrite(motorR1, 700);
+  digitalWrite(motorR2, LOW);
+  
+}
+ 
+void lull() {
+  digitalWrite(motorL1, HIGH);
+  digitalWrite(motorL2, HIGH);
+  digitalWrite(motorR1, HIGH);
+  digitalWrite(motorR2, HIGH);
+  
+}
